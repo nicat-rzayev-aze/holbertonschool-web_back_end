d4195ff534737299c75631ca0b3790c0781db0c9
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-12 00:15:11 +0400

    Create 1-simple_pagination.py

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Find the correct indexes to paginate the dataset correctly.
+    
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items per page.
+        
+    Returns:
+        tuple: A tuple containing the start index and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Return the appropriate page of the dataset.
+        
+        Args:
+            page (int): The page number, must be greater than 0.
+            page_size (int): The page size, must be greater than 0.
+            
+        Returns:
+            List[List]: The list of rows for the page, or an empty list 
+                        if out of range.
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        data = self.dataset()
+        start, end = index_range(page, page_size)
+
+        if start >= len(data):
+            return []
+
+        return data[start:end]
